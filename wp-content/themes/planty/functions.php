53fce4e4fd4a720c26ab452bd6c776b146fe70b2
Aymeric Bienfait <Camy.nodream@hotmail.fr>
2023-08-29 17:44:20 +0200

    Pluging, header & admin ok, page home

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -8,7 +8,8 @@ function theme_enqueue_styles()
      . '/css/theme.css'));
 }
 
-/* Header */
+/* Header & Footer personnalisés : le thème Neve convenant dans le projet actuel, je laisse les deux fonctions à titre d'exemple */
+/*
 function register_custom_menus() {
     register_nav_menus(array(
         'header-menu' => 'Menu du header',
@@ -16,17 +17,19 @@ function register_custom_menus() {
 }
 add_action('after_setup_theme', 'register_custom_menus');
 
-/* Footer */
+
 function register_footer_menu() {
     register_nav_menu('footer-menu', 'Menu du footer');
 }
-add_action('after_setup_theme', 'register_footer_menu');
+add_action('after_setup_theme', 'register_footer_menu'); 
+*/
+
 
 /* Admin link */
 add_filter('wp_nav_menu_items', 'ajouter_lien_admin', 10, 2);
 function ajouter_lien_admin($items, $args) {
-    if (is_user_logged_in() && current_user_can('administrator')) {
-        $items .= '<li><a href="#">Admin</a></li>';
+    if ($args->theme_location == 'primary' && is_user_logged_in() && current_user_can('administrator')) {
+        $items .= '<li><a href="' . admin_url() . '">Admin</a></li>';
     }
     return $items;
-} 
+}
